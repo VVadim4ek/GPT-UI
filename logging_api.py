de39158d8ed507367bac1dc3eedf614bf0dbcc25
Vadym <vadim.karapuz@gmail.com>
2024-07-23 12:30:40 +0900

    badge

--- logging_api.py
+++ logging_api.py
@@ -21,7 +21,7 @@ class LoggerConfiguration:
         """
         log_file_path = Path(__file__).parent / filename
 
-        # Remove any existing handlers if already configured@genie559
+        # Remove any existing handlers if already configure
         for handler in logging.root.handlers[:]:
             logging.root.removeHandler(handler)
 
